38ab041aaf334297c25a741f257072154c364958
changDDAO <younch8342@naver.com>
2025-01-21 21:33:33 +0900

    feat: React를 이용한 TicTaeToe 게임 만들기1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import './App.css'
-import MyButton from "./components/MyButton.jsx";
-import {useState} from "react";
+import Board from "./tictaetoe/Board.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () =>{
-    setCount(count => count + 1);
-  }
+
   return (
     <>
-      {/*<Profile/>*/}
-        {/*<ShoppingList/>*/}
-        {/*각 컴퍼넌트들이 자율적으로 Count의 값이 상태관리 되는 것에 집중하자.*/}
-        <MyButton count={count} onClick={handleClick}/>
-        <MyButton count={count} onClick={handleClick}/>
-
+    <Board/>
   </>
   )
 }
